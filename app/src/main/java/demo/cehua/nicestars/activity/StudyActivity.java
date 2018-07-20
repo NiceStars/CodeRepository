79ab6ddc219e3387d7930a1566a0001acd734bf2
NiceStars <nicestars@qq.com>
2018-07-20 11:15:53 +0800

    dev提交

---
+++ app/src/main/java/demo/cehua/nicestars/activity/StudyActivity.java
@@ -0,0 +1,25 @@
+package demo.cehua.nicestars.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+/**
+ * Time:2018/7/20
+ * Anthor:NiceStars
+ * Description:
+ */
+public class StudyActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_study);
+        this.getMainLooper();
+
+        Toast.makeText(this, "this.dev branch", Toast.LENGTH_SHORT).show();
+
+    }
+}
